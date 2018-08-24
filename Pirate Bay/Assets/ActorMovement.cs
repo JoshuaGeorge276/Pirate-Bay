868701e508f6cb709c3c9269420216b75fe1fd5c
Alex Kitching <alexkitching@gmail.com>
2018-08-24 18:54:00 +0100

    - Added Alex Test Scene - Added Input, Behaviour and Event System - Reorganised Folders - Added some Sprites

---
+++ Pirate Bay/Assets/ActorMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Core;
+using UnityEngine;
+
+public class ActorMovement : ManagedBehaviour
+{
+    private InputPlayer inputPlayer;
+
+    void Awake()
+    {
+
+    }
+
+	// Use this for initialization
+	protected override void Start () 
+	{
+		base.Start();
+	    inputPlayer = InputManager.Instance.GetPlayer(0);
+	}
+
+    public override void ManagedUpdate(float a_fDeltaTime)
+    {
+        Vector2 currentPos = transform.position;
+
+        float x = inputPlayer.Controller.Device.GetAxis(InputAxisValue.LeftX);
+        float y = inputPlayer.Controller.Device.GetAxis(InputAxisValue.LeftY);
+
+        currentPos.x += x * a_fDeltaTime;
+        currentPos.y += y * a_fDeltaTime;
+
+
+        transform.position = currentPos;
+    }
+}
---
+++ Pirate Bay/Assets/PirateBay/Scripts/Core/Input/IDeviceHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public interface IDeviceHandler
+{
+    void ResetDevice();
+    void UpdateDevice();
+    void LateUpdateDevice();
+    float GetAxis(InputAxisValue a_value);
+    bool GetButtonDown(InputButtonValue a_value);
+    bool GetButtonPressed(InputButtonValue a_value);
+    bool GetButtonUp(InputButtonValue a_value);
+}
