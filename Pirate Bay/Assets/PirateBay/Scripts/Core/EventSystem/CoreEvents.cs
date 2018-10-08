57c669bc62c5cbef87a72c4cd58f8950917420f7
TEAM17\akitching <akitching@team17.com>
2018-10-08 13:03:34 +0100

    - Updated Project to use C#7 and Rosyln Compiler - Added Event System from Planet Red - Updated Git Ignore

---
+++ Pirate Bay/Assets/PirateBay/Scripts/Core/EventSystem/CoreEvents.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.EventSystem.CoreEvents
+{
+    public class ExampleTestEvent : GameEvent
+    {
+        public string text = "ExampleTestEvent";
+    }
+}
