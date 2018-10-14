dc175aea28e8fb69e368026e1f40ec4284e438b2
Joshua George <joshuageorge276@gmail.com>
2018-10-14 16:15:53 +0100

    Created an Island scene with new Tilesets
    Also changed the Player sprites to 16 pixels per unit to better match the size of the tilemap and filter mode from bilinear to point so that it appears sharper around the edges.

--- Pirate Bay/Assets/PirateBay/Scripts/Post Processing/BattleTransitions.cs
+++ Pirate Bay/Assets/PirateBay/Scripts/Post Processing/BattleTransitions.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class BattleTransitions : MonoBehaviour {
 
+    public Color colour;
     public Texture directionTexture;
     public Texture transitionTexture;
+    public Texture transitionToGameTexture;
+
+    [Range(0, 5)]
+    public int flashCount = 2;
+    public float flashSpeed = 3.0f;
+    public float fadeDuration = 0.2f;
+    public float transitionSpeed = 1.0f;
 
     private Material mat;
 
@@ -13,9 +21,8 @@ public class BattleTransitions : MonoBehaviour {
     {
         mat = new Material(Shader.Find("Custom/BattleTransition"));
         mat.SetTexture("_DirTex", directionTexture);
-        mat.SetTexture("_TransitionTexture", transitionTexture);
-
-        StartCoroutine(Pulse(3, 0.5f));
+        mat.SetTexture("_TransitionTex", transitionTexture);
+        mat.SetColor("_Color", colour);
     }
 
     IEnumerator Pulse(float speed, float fadeInTime)
@@ -40,10 +47,70 @@ public class BattleTransitions : MonoBehaviour {
         float fade = 1;
         while (fade > 0)
         {
-            fade -= Time.deltaTime * speed;
+            fade -= Time.smoothDeltaTime * speed;
             mat.SetFloat("_Fade", fade);
             yield return null;
         }
+    }
+
+    IEnumerator TransitionToBlack(float speed, bool distort)
+    {
+        mat.SetTexture("_TransitionTex", transitionTexture);
+        mat.SetFloat("_Fade", 0);
+        mat.SetFloat("_Distort", (distort) ? 1 : 0);
+
+        float cutoff = 0;
+
+        while(cutoff < 1)
+        {
+            cutoff += Time.smoothDeltaTime * speed;
+            mat.SetFloat("_Cutoff", cutoff);
+            yield return null;
+        }
+
+    }
+
+    IEnumerator TransitionToGame(float speed)
+    {
+        mat.SetTexture("_TransitionTex", transitionToGameTexture);
+        mat.SetFloat("_Fade", 0);
+
+        float cutoff = 1;
+
+        while (cutoff > 0)
+        {
+            cutoff -= Time.smoothDeltaTime * speed;
+            mat.SetFloat("_Cutoff", cutoff);
+            yield return null;
+        }
+
+    }
+
+    IEnumerator Play(int flashCount, float fadeSpeed = 1f, float fadeDuration = 0.5f, float transitionSpeed = 1f)
+    {
+        for(int i = 0; i < flashCount; i++)
+        {
+            StartCoroutine(Pulse(fadeSpeed, fadeDuration));
+            yield return new WaitForSeconds(fadeDuration + 0.5f);
+        }
+
+        StartCoroutine(TransitionToBlack(transitionSpeed, false));
+
+        yield return new WaitForSeconds(3.0f);
+
+        StartCoroutine(TransitionToGame(1.5f));
+    }
+
+    [ContextMenu("Play Battle Transition")]
+    private void Play()
+    {
+        if (mat == null)
+        {
+            Debug.LogError("Must be in play mode!");
+            return;
+        }
+
+        StartCoroutine(Play(flashCount, flashSpeed, fadeDuration, transitionSpeed));
     }
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
